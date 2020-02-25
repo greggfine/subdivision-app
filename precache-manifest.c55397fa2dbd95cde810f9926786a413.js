73b1af961fc39c352757561a5db81be784e7577a
Gregg Fine <bbeez8500@gmail.com>
2020-02-25 15:27:26 -0500

    Updates

---
+++ precache-manifest.c55397fa2dbd95cde810f9926786a413.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf4fb54411ce43b00f9581666618b71",
+    "url": "/subdivision-app/index.html"
+  },
+  {
+    "revision": "8e1d6293d7b81d35ecaa",
+    "url": "/subdivision-app/static/css/2.61f7f2c0.chunk.css"
+  },
+  {
+    "revision": "dbdff9362359dd5cad53",
+    "url": "/subdivision-app/static/css/main.2f5deca4.chunk.css"
+  },
+  {
+    "revision": "8e1d6293d7b81d35ecaa",
+    "url": "/subdivision-app/static/js/2.6def3fa9.chunk.js"
+  },
+  {
+    "revision": "0c4d2ae2ebe481529bf53445966b226a",
+    "url": "/subdivision-app/static/js/2.6def3fa9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbdff9362359dd5cad53",
+    "url": "/subdivision-app/static/js/main.a09c3644.chunk.js"
+  },
+  {
+    "revision": "22cb21b6709e04e2bab4",
+    "url": "/subdivision-app/static/js/runtime-main.dffcda59.js"
+  },
+  {
+    "revision": "0ec2583d8f978ea12c84962e10098c53",
+    "url": "/subdivision-app/static/media/arrows-h.0ec2583d.svg"
+  },
+  {
+    "revision": "e67b76812312f686f0d551b8262cbe65",
+    "url": "/subdivision-app/static/media/failure.e67b7681.mp3"
+  },
+  {
+    "revision": "f8f082f3fa4ca5ade2541a6d45adad79",
+    "url": "/subdivision-app/static/media/game_over.f8f082f3.mp3"
+  },
+  {
+    "revision": "86e97790aa1d438f8ff0b7953883a15d",
+    "url": "/subdivision-app/static/media/guitar-4510136_1920_resized.86e97790.png"
+  },
+  {
+    "revision": "d186d2cb1d852819b4bf1a1d209c97b7",
+    "url": "/subdivision-app/static/media/loop3.d186d2cb.mp3"
+  },
+  {
+    "revision": "d7524f83d47f6cde1022ae78ff975a42",
+    "url": "/subdivision-app/static/media/loop7.d7524f83.mp3"
+  },
+  {
+    "revision": "2c3a8ee4f6d841ecc64e709496864805",
+    "url": "/subdivision-app/static/media/notation1.2c3a8ee4.jpg"
+  },
+  {
+    "revision": "8ec894b1a486d020f34f18049ff01d76",
+    "url": "/subdivision-app/static/media/notation2.8ec894b1.jpg"
+  },
+  {
+    "revision": "aac215623c52c0ba3ec338e2c15234a5",
+    "url": "/subdivision-app/static/media/play.aac21562.svg"
+  },
+  {
+    "revision": "48905b5e7a52d91bc54dfbd71e526154",
+    "url": "/subdivision-app/static/media/refresh.48905b5e.svg"
+  },
+  {
+    "revision": "9886c912599194586949045153f7d943",
+    "url": "/subdivision-app/static/media/silhouette-photo-of-man-singing-on-stage-1916824.9886c912.jpg"
+  },
+  {
+    "revision": "e3f280098995150f5c297209ea5b84aa",
+    "url": "/subdivision-app/static/media/step-backward.e3f28009.svg"
+  },
+  {
+    "revision": "1d958c3ab4c19ae6f7fbf03b098e06c6",
+    "url": "/subdivision-app/static/media/success.1d958c3a.mp3"
+  }
+]);
